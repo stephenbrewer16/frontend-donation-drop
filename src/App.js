365130a09836d93ddaff42d9047da2c05f07cf66
Stephen Brewer <stephebrewer16@gmail.com>
2019-12-22 19:06:59 -0500

    signup imported

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom'
+import Signup from './Components/signup'
 
 export default class App extends Component {
   state = {
