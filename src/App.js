ab5a9ab6d7b2e3cee79fd3ef89c672dadb9391ab
Stephen Brewer <stephebrewer16@gmail.com>
2019-12-22 19:14:08 -0500

    imported main container

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import Signup from './Components/signup'
 import login_page from './Components/login_page'
 import profile_page from './Components/profile_page'
+import Main from './Containers/Main'
 
 export default class App extends Component {
   state = {
