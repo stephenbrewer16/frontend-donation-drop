33c64866f9027d52ad0cc555da123424712e737a
Stephen Brewer <stephebrewer16@gmail.com>
2019-12-22 19:12:42 -0500

    added profile page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import { Route, Switch } from 'react-router-dom'
 import Signup from './Components/signup'
 import login_page from './Components/login_page'
+import profile_page from './Components/profile_page'
 
 export default class App extends Component {
   state = {
