f0911222248e0492e9f0b2c4105f7e9b253cd187
Stephen Brewer <stephebrewer16@gmail.com>
2019-12-22 19:09:31 -0500

    login page imported

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import { Route, Switch } from 'react-router-dom'
 import Signup from './Components/signup'
+import login_page from './Components/login_page'
 
 export default class App extends Component {
   state = {
